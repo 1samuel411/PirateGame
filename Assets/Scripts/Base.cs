2aa08ee8b55907074b112490162a2c3cccca6164
Samuel Arminana <armi.sam99@gmail.com>
2018-02-08 16:59:46 -0500

    Entity begin on laptop

---
+++ Assets/Scripts/Base.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Base : MonoBehaviour
+{
+	
+    private Transform _transform;
+    [HideInInspector]
+    public Transform transform
+    {
+	    get
+        {
+            if(_transform == null)
+            {
+                _transform = GetComponent<Transform>();
+            }
+
+            return _transform;
+        }
+    }
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Scripts/Player/EntityHumanoid.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace PirateGame.Entity
+{
+	public class EntityHumanoid : Entity
+	{
+	
+	    void Start()
+	    {
+			
+	    }
+	
+	    void Update()
+	    {
+	
+	    }
+	}
+}
