c6c389a8e5649c6c7db9641ccbaca39ce6cd5224
David Debnar <davidd51197@gmail.com>
2018-02-10 10:38:44 +0100

    Added buoyancy toolkit and buoyancy to the ship and water masking for hull interior

---
+++ Assets/Scripts/Graphics/SetRenderQueue.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+ 
+[RequireComponent(typeof(MeshRenderer))]
+public class SetRenderQueue : MonoBehaviour
+{
+	[SerializeField]
+	protected int[] m_queues = new int[]{3000};
+ 
+	protected void Awake()
+	{
+		Material[] materials = GetComponent<MeshRenderer>().materials;
+		
+		for (int i = 0; i < materials.Length && i < m_queues.Length; ++i)
+		{
+			materials[i].renderQueue = m_queues[i];
+		}
+	}
+}
---
+++ Assets/Scripts/Physics/SetCenterOfMass.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace PirateGame
+{
+	public class SetCenterOfMass : MonoBehaviour
+	{
+		public Transform CenterOfMass;
+	
+		protected void FixedUpdate()
+		{
+			if (CenterOfMass != null)
+				GetComponent<Rigidbody>().centerOfMass = transform.InverseTransformPoint(CenterOfMass.position);
+		}
+	}
+}
